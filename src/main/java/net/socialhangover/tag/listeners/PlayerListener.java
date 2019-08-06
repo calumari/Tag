61eee81f3b949fa47f2ddd298f4ebe7b939fc2bd
2277 <38501234+2277@users.noreply.github.com>
2019-08-06 04:19:05 +0100

    Create PlayerListener.java

---
+++ src/main/java/net/socialhangover/tag/listeners/PlayerListener.java
@@ -0,0 +1,41 @@
+package net.socialhangover.tag.listeners;
+
+import lombok.RequiredArgsConstructor;
+import net.socialhangover.tag.TagPlugin;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+@RequiredArgsConstructor
+public class PlayerListener implements Listener {
+
+    private final TagPlugin plugin;
+
+    @EventHandler
+    public void onPlayerJoinEvent(PlayerJoinEvent event) {
+        if (plugin.getConfiguration().TAGGED_PLAYER.get() == null) {
+            plugin.tag(event.getPlayer());
+            PlayerJoinEvent.getHandlerList().unregister(this);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerRespawnEvent(PlayerRespawnEvent event) {
+        if (plugin.isTagged(event.getPlayer())) { plugin.applyTag(event.getPlayer()); }
+    }
+
+    @EventHandler
+    public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Player && event.getEntity() instanceof Player) {
+            Player damager = (Player) event.getDamager();
+            if (!plugin.isTagged(damager) || !plugin.canTag()) { return; }
+            plugin.removeTag(damager);
+            plugin.tag((Player) event.getEntity());
+            event.setDamage(0);
+        }
+    }
+
+}
